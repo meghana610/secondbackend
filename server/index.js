b974c70726c7823b6852fb6226d6266d3ab34ab2
meghana610 <devatameghana610@gmail.com>
2025-08-12 15:42:10 +0530

    backend projects

---
+++ server/index.js
@@ -0,0 +1,21 @@
+console.log("This is Meghana")
+
+let fname = "Rama"
+let lname = "Meghana"
+
+console.log(fname+lname);
+
+let a = 10;
+let b = "Meghana"
+let c = false
+let d = null;
+
+
+let person ={
+    fname:"Meghana",
+    isAlive:true,
+    college:null
+}
+
+let students = ["akhi","akhila","maggie"]
+console.log("students")
